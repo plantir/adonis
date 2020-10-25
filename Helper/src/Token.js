2facd890f6cfa00eb45c04876e6dd2152f2c9d2d
armin <arminkheirkhahan@gmail.com>
2020-10-25 14:51:49 +0330

    change token generator

--- Helper/src/Token.js
+++ Helper/src/Token.js
@@ -1,19 +1,16 @@
-'use strict';
+"use strict";
 class TokenGenerator {
   static generate(digits = 6) {
-    let token = (
-      Math.floor(Math.random() * 10 * parseInt(digits)) +
-      10 * parseInt(digits)
-    )
+    let token = Math.random()
       .toString()
-      .substring(1);
+      .slice(2, digits + 2);
     return token;
   }
   static uid() {
     var length = 14;
-    let token = '';
+    let token = "";
     var possible =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     for (var i = 0; i < length; i++) {
       token += possible.charAt(Math.floor(Math.random() * possible.length));
     }
